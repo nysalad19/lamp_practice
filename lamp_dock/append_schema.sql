778ba5dee7292f66c31262c5c317384f0c2b8119
EC2 Default User <ec2-user@ip-172-31-28-12.ec2.internal>
2021-05-04 15:56:16 +0900

    テーブルの作成

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE history (
+	order_id INT(11) AUTO_INCREMENT,
+	purchased DATETIME NOT NULL,
+	user_id INT(11) NOT NULL,
+	primary key(order_id)
+);
+
+CREATE TABLE details (
+	order_id INT(11) AUTO_INCREMENT,
+	item_id INT(11) NOT NULL,
+	price INT(11) NOT NULL,
+	amount INT(11) NOT NULL,
+	primary key(order_id)
+);
